85eced1f80b85eb210f231195302965b6eebfbdd
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2026-05-28 21:00:40 -0300

    feat: criação do menu inicial

---
+++ main.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+print("BEM VINDO AO DEVERY NIGHT, SEU SISTEMA DE GESTÃO PARA CONVÊNIENCIA")
+
+opcao = ""
+print("""
+============================
+======= DEVERY NIGHT =======
+============================
+
+  [1] MÓDULO PRODUTOS
+  [2] MÓDULO CLIENTES
+  [3] MÓDULO VENDAS
+  [4] MÓDULO RELATÓRIOS
+  [5] MÓDULO INFORMAÇÕES
+  [0] SAIR
+  
+""")
+sleep(1)
+opcao = input("digite sua opção: "))
